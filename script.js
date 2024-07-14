427bb9eeee23fd46d53c8dc2125c77fa6a8a1b09
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-14 11:36:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const bandElement = document.getElementById('band');
+let list = '';
+bands.forEach(b => {
+    list += `<li>${b}</li>`;
+});
+
+bandElement.innerHTML = list; 
