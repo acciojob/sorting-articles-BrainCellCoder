2a5d459dbed202f566a2c484fa9f7a9b4cccfad7
BrainCellCoder <70462882+BrainCellCoder@users.noreply.github.com>
2024-07-14 11:40:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-//your JS code here. If required.
-
 const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
 
 const bandElement = document.getElementById('band');
+function stripArticle(bandName) {
+    return bandName.replace(/^(a |an |the )/i, '').trim();
+}
+const sortedBands = bands.sort((a, b) => {
+    const bandA = stripArticle(a);
+    const bandB = stripArticle(b);
+    return bandA.localeCompare(bandB);
+});
 let list = '';
-bands.forEach(b => {
+sortedBands.forEach(b => {
     list += `<li>${b}</li>`;
 });
 
-bandElement.innerHTML = list; 
+bandElement.innerHTML = list;
